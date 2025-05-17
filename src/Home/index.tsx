c63070063655171888fb0041a4dff0337448c01a
paulo-beltramin <dev.beltramin@gmail.com>
2025-05-17 18:35:21 -0300

    desenvolvido comunicação com api  e função para mostrar mais moedas

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -1,12 +1,70 @@
+import { Link, useNavigate } from "react-router"
+import { useState, type FormEvent, useEffect } from "react"
 import { BsSearch } from "react-icons/bs"
 
 import style from './style.module.scss'
-import { Link, useNavigate } from "react-router"
-import { useState, type FormEvent } from "react"
+
+type bitProps = {
+  id: string,
+  name: string,
+  symbol: string,
+  marketCapUsd: string,
+  priceUsd: number,
+  volumeUsd24Hr: string,
+  changePercent24Hr: string,
+  formatedPrice?: string,
+  priceCompact?: string,
+  volumePrice?: string
+}
+
+type dataProps = {
+  data: bitProps[]
+}
+
 
 export const Home = () => {
   const [input, setInput] = useState('')
+  const [coins, setCoins] = useState<bitProps[]>([])
+  const [offset, setOffSet] = useState(0)
 
+  useEffect(() => {
+    getDataCoins()
+  }, [offset])
+
+
+  const getDataCoins = async () => {
+    fetch(`https://rest.coincap.io/v3/assets?limit=20&offset=${offset}&apiKey=d5cd8b5f4795ffd9c12b1a43f37ce62c0a81b28228663228abb8c4a0f5f444dc`)
+      .then((res) => res.json()
+      )
+      .then((data: dataProps) => {
+        const dados = data.data
+
+        const price = Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD'
+        })
+
+        const compactPrice = Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+          notation: 'compact'
+        })
+
+        const formaterPrice = dados.map(item => {
+          const priceFormater = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+            priceCompact: compactPrice.format(Number(item.marketCapUsd)),
+            volumePrice: compactPrice.format(Number(item.volumeUsd24Hr))
+          }
+
+          return priceFormater
+        })
+
+        setCoins([...coins, ...formaterPrice]
+      })
+
+  }
   const navigate = useNavigate()
 
   const handleSubmit = (e: FormEvent) => {
@@ -15,6 +73,16 @@ export const Home = () => {
 
     navigate(`/detail/${input}`)
 
+  }
+
+  const getMoreCoins = () => {
+
+    if (offset === 0) {
+      setOffSet(10)
+      return
+    }
+
+    setOffSet(offset + 10)
   }
 
   return (
@@ -38,18 +106,28 @@ export const Home = () => {
           </thead>
 
           <tbody>
-            <tr>
-              <td><Link to={''}>Bitcoin - BTC</Link></td>
-              <td>1T</td>
-              <td>8.000</td>
-              <td>2B</td>
-              <td className={style.container_table__positivo}>1,20</td>
-            </tr>
+            {coins.map(item => (
+              <>
+                <tr key={item.id}>
+                  <div className={style.container_table__logo}>
+                    <img src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`} alt="logo cripto moeda" />
+                    <td><Link to={''}>{item.name} | {item.symbol}</Link></td>
+                  </div>
+                  <td>{item.priceCompact}</td>
+                  <td>{item.formatedPrice}</td>
+                  <td>{item.volumePrice}</td>
+                  <td className={Number(item.changePercent24Hr) > 0 ?
+                    style.container_table__positivo : style.container_table__negativo
+
+                  }>{Number(item.changePercent24Hr).toFixed(2)}</td>
+                </tr >
+              </>
+            ))}
           </tbody>
         </table>
 
-        <button>Ver +</button>
-      </section>
+        <button onClick={getMoreCoins}>Ver +</button>
+      </section >
     </>
   )
 }
